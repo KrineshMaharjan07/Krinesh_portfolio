90a930985a613b38917e566b917ca13b15deb77e
Krinesh Maharjan <141649561+KrineshMaharjan07@users.noreply.github.com>
2025-05-03 15:57:37 +0545

    update next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  basePath: '/portfolio',
+  // DO NOT add: output: 'export' unless doing a static export
+};
 
-}
+module.exports = nextConfig;
 
-export default nextConfig
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   basePath: '/portfolio',
+
+// }
+
+// export default nextConfig
